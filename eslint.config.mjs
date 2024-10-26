17606504eab96d4ce64c0bf0bd223c56091aa6bd
Gustav <gustav_lowen@hotmail.com>
2024-10-26 10:09:14 +0200

    add mui package

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,7 @@
 import globals from "globals";
 import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
+import prettier from "eslint-config-prettier";
 
 const recommendedConfig = [...tseslint.configs.recommended];
 
@@ -23,5 +24,6 @@ export default [
   { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
   { languageOptions: { globals: globals.browser } },
   ...recommendedConfig,
+  prettier,
   pluginReact.configs.flat.recommended
 ];
