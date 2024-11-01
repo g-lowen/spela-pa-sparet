52871a9b6528b13e27d5cadbbf442f3de01891f9
Gustav <gustav_lowen@hotmail.com>
2024-11-01 15:30:44 +0100

    add latest update text

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
 import "./App.css";
 import DataTable from "./components/DataTable";
-
+import { MATCHES } from "./constants/constants";
 function App() {
-  return <DataTable />;
+  const latestMatch = MATCHES?.filter((match) => match.winner !== null).at(-1);
+  return (
+    <>
+      <center>
+        Senaste uppdateringen:{" "}
+        {latestMatch ? latestMatch.date : "Inga matcher spelade"}
+      </center>
+      <DataTable />
+    </>
+  );
 }
 
 export default App;
