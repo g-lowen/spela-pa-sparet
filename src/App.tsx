eb9f3bd9cace023c9e1a46c736d7122baf9f5831
g-lowen <gustav_lowen@hotmail.com>
2025-10-19 22:10:41 +0200

    add tabs and group 3 and 4

--- src/App.tsx
+++ src/App.tsx
@@ -7,66 +7,74 @@ import DataTable from "./components/DataTable";
 import { Header } from "./components/Header";
 import { MATCHES } from "./constants/matches";
 import { CardsContainer } from "./components/CardsContainer";
+import { TabPanel } from "./components/TabPanel";
 
 function App() {
-  const [showTable, setShowTable] = useState(false);
+  const [tabValue, setTabValue] = useState(0);
+
+  const handleChange = (_event: React.SyntheticEvent, newValue: number) => {
+    setTabValue(newValue);
+  };
 
   const upcomingMatch = MATCHES.find((match) => match.winner === null);
-  const groupOne = MATCHES.filter((_match, index) => index < 6);
-  const groupTwo = MATCHES.filter((_match, index) => index >= 6 && index < 12);
+  const groupOne = MATCHES.filter((_match, index) => index < 3);
+  const groupTwo = MATCHES.filter((_match, index) => index >= 3 && index < 6);
+  const groupThree = MATCHES.filter((_match, index) => index >= 6 && index < 9);
+  const groupFour = MATCHES.filter((_match, index) => index >= 9 && index < 12);
   const playoff = MATCHES.filter(
     (match) => match.matchType === "semifinal" || match.matchType === "final"
   );
 
   return (
     <Box>
-      <Header />
+      <Header onChange={handleChange} tabValue={tabValue} />
+
       <center style={{ padding: "6px" }}>
-        {upcomingMatch ? (
-          <>
-            <h1>Nästa avsnitt</h1>
-            <ChartCard
-              match={upcomingMatch}
-              matchIndex={upcomingMatch && MATCHES.indexOf(upcomingMatch)}
-              sx={{ maxWidth: "355px" }}
-            />
-          </>
-        ) : null}
-        <Box
-          sx={{
-            alignItems: "center",
-            display: "flex",
-            gap: "6px",
-            justifyContent: "center"
-          }}
-        >
-          <h2>Tabell</h2>
-          <Button
-            onClick={() => setShowTable((prev) => !prev)}
-            variant="contained"
-          >
-            {showTable ? "Göm" : "Visa"}
-          </Button>
-        </Box>
-        <DataTable showTable={showTable} />
-        <h2>Första gruppen</h2>
-        <CardsContainer>
-          {groupOne.map((match, index) => (
-            <ChartCard key={index} match={match} matchIndex={index} />
-          ))}
-        </CardsContainer>
-        <h2>Andra gruppen</h2>
-        <CardsContainer>
-          {groupTwo.map((match, index) => (
-            <ChartCard key={index} match={match} matchIndex={index + 6} />
-          ))}
-        </CardsContainer>
-        <h2>Slutspel</h2>
-        <CardsContainer>
-          {playoff.map((match, index) => (
-            <ChartCard key={index} match={match} matchIndex={index + 12} />
-          ))}
-        </CardsContainer>
+        <TabPanel value={tabValue} index={0}>
+          {upcomingMatch ? (
+            <>
+              <h1>Nästa avsnitt</h1>
+              <ChartCard
+                match={upcomingMatch}
+                matchIndex={upcomingMatch && MATCHES.indexOf(upcomingMatch)}
+                sx={{ maxWidth: "355px" }}
+              />
+            </>
+          ) : null}
+          <h2>Första gruppen</h2>
+          <CardsContainer>
+            {groupOne.map((match, index) => (
+              <ChartCard key={index} match={match} matchIndex={index} />
+            ))}
+          </CardsContainer>
+          <h2>Andra gruppen</h2>
+          <CardsContainer>
+            {groupTwo.map((match, index) => (
+              <ChartCard key={index} match={match} matchIndex={index + 3} />
+            ))}
+          </CardsContainer>
+          <h2>Tredje gruppen</h2>
+          <CardsContainer>
+            {groupThree.map((match, index) => (
+              <ChartCard key={index} match={match} matchIndex={index + 6} />
+            ))}
+          </CardsContainer>
+          <h2>Fjärde gruppen</h2>
+          <CardsContainer>
+            {groupFour.map((match, index) => (
+              <ChartCard key={index} match={match} matchIndex={index + 9} />
+            ))}
+          </CardsContainer>
+          <h2>Slutspel</h2>
+          <CardsContainer>
+            {playoff.map((match, index) => (
+              <ChartCard key={index} match={match} matchIndex={index + 12} />
+            ))}
+          </CardsContainer>
+        </TabPanel>
+        <TabPanel value={tabValue} index={1}>
+          <DataTable />
+        </TabPanel>
       </center>
     </Box>
   );
--- src/components/DataTable.tsx
+++ src/components/DataTable.tsx
@@ -5,75 +5,58 @@ import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
-import Skeleton from "@mui/material/Skeleton";
 import { GAMBLERS } from "../constants/gamblers";
 import { createRowData } from "./helpers/createRowData";
 import { Row } from "./Row";
 
 import useMediaQuery from "@mui/material/useMediaQuery";
 
-interface DataTableProps {
-  showTable: boolean;
-}
-
-export default function DataTable({ showTable }: DataTableProps) {
+export default function DataTable() {
   const isSmallScreen = useMediaQuery("(max-width:600px)");
 
   return (
-    <>
-      {showTable ? (
-        <Paper
-          sx={{
-            maxWidth: "960px",
-            width: "100%"
-          }}
-        >
-          <TableContainer
-            sx={{
-              ...(isSmallScreen && {
-                ["& .MuiTableCell-root"]: {
-                  padding: "6px 2px",
-                  ["&:last-of-type:not(:first-of-type)"]: {
-                    padding: "6px"
-                  },
-                  ["&:first-of-type:not(:first-of-type:has(.MuiCollapse-root))"]:
-                    {
-                      padding: "6px"
-                    }
-                }
-              })
-            }}
-          >
-            <Table aria-label="collapsible table" size="small">
-              <TableHead>
-                <TableRow>
-                  <TableCell width="20" />
-                  <TableCell>Namn</TableCell>
-                  <TableCell align="right">Matcher</TableCell>
-                  <TableCell align="right">✔️</TableCell>
-                  <TableCell align="right">❌</TableCell>
-                  <TableCell align="right">Poäng</TableCell>
-                </TableRow>
-              </TableHead>
-              <TableBody>
-                {rows
-                  .sort((a, b) => b.points - a.points)
-                  .map((row) => (
-                    <Row key={row.name} row={row} />
-                  ))}
-              </TableBody>
-            </Table>
-          </TableContainer>
-        </Paper>
-      ) : (
-        <Skeleton
-          height={343}
-          width="100%"
-          sx={{ borderRadius: "5px", maxWidth: "960px" }}
-          variant="rectangular"
-        />
-      )}
-    </>
+    <Paper
+      sx={{
+        maxWidth: "960px",
+        width: "100%",
+      }}
+    >
+      <TableContainer
+        sx={{
+          ...(isSmallScreen && {
+            ["& .MuiTableCell-root"]: {
+              padding: "6px 2px",
+              ["&:last-of-type:not(:first-of-type)"]: {
+                padding: "6px",
+              },
+              ["&:first-of-type:not(:first-of-type:has(.MuiCollapse-root))"]: {
+                padding: "6px",
+              },
+            },
+          }),
+        }}
+      >
+        <Table aria-label="collapsible table" size="small">
+          <TableHead>
+            <TableRow>
+              <TableCell width="20" />
+              <TableCell>Namn</TableCell>
+              <TableCell align="right">Matcher</TableCell>
+              <TableCell align="right">✔️</TableCell>
+              <TableCell align="right">❌</TableCell>
+              <TableCell align="right">Poäng</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {rows
+              .sort((a, b) => b.points - a.points)
+              .map((row) => (
+                <Row key={row.name} row={row} />
+              ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </Paper>
   );
 }
 
