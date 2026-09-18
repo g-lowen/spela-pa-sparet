e839c431a4edf133c68fc4428637e5d4bf45a690
g-lowen <gustav_lowen@hotmail.com>
2026-09-18 20:59:49 +0200

    update gamlbers

--- src/constants/seasons/season-2026/gamblers.ts
+++ src/constants/seasons/season-2026/gamblers.ts
@@ -1,9 +1,501 @@
 import { Gambler } from "../../../types";
 import { Season2026Team } from "./teams";
 
-/**
- * Generated from the form responses:
- *   node scripts/parse-csv.js <input.csv> src/constants/seasons/season-2026/gamblers.ts
- * Remember to re-point the import paths and the team type after regenerating.
- */
-export const GAMBLERS: Gambler<Season2026Team>[] = [];
+export const GAMBLERS: Gambler<Season2026Team>[] = [
+  {
+    name: "Gustav",
+    bets: [
+      {
+        matchType: "group",
+        winner: "Lotta Lundgren & Erik Haag",
+      },
+      {
+        matchType: "group",
+        winner: "Lotta Lundgren & Erik Haag",
+      },
+      {
+        matchType: "group",
+        winner: "Gry Forssell & Jonas Rhodiner",
+      },
+      {
+        matchType: "group",
+        winner: "Isobel Hadley-Kamptz & Kalle Lind",
+      },
+      {
+        matchType: "group",
+        winner: "Malin Persson Giolito & Ola Söderholm",
+      },
+      {
+        matchType: "group",
+        winner: "Malin Persson Giolito & Ola Söderholm",
+      },
+      {
+        matchType: "group",
+        winner: "Kirsty Armstrong & Hanna Lublin Niklasson",
+      },
+      {
+        matchType: "group",
+        winner: "Kirsty Armstrong & Hanna Lublin Niklasson",
+      },
+      {
+        matchType: "group",
+        winner: "Andrew Walden & Kristoffer ”K” Svensson",
+      },
+      {
+        matchType: "group",
+        winner: "Amy Deasismont & Uje Brandelius",
+      },
+      {
+        matchType: "group",
+        winner: "Amy Deasismont & Uje Brandelius",
+      },
+      {
+        matchType: "group",
+        winner: "Jakob Norrgård & Axel Åhman",
+      },
+      {
+        matchType: "semifinal",
+        semifinalFirst: "Kirsty Armstrong & Hanna Lublin Niklasson",
+        semifinalSecond: "Malin Persson Giolito & Ola Söderholm",
+      },
+      {
+        matchType: "final",
+        winner: "Kirsty Armstrong & Hanna Lublin Niklasson",
+      },
+    ],
+  },
+  {
+    name: "Gul tre skilling banco",
+    bets: [
+      {
+        matchType: "group",
+        winner: "Lotta Lundgren & Erik Haag",
+      },
+      {
+        matchType: "group",
+        winner: "Lotta Lundgren & Erik Haag",
+      },
+      {
+        matchType: "group",
+        winner: "Gry Forssell & Jonas Rhodiner",
+      },
+      {
+        matchType: "group",
+        winner: "Isobel Hadley-Kamptz & Kalle Lind",
+      },
+      {
+        matchType: "group",
+        winner: "Malin Persson Giolito & Ola Söderholm",
+      },
+      {
+        matchType: "group",
+        winner: "Isobel Hadley-Kamptz & Kalle Lind",
+      },
+      {
+        matchType: "group",
+        winner: "Andrew Walden & Kristoffer ”K” Svensson",
+      },
+      {
+        matchType: "group",
+        winner: "Kirsty Armstrong & Hanna Lublin Niklasson",
+      },
+      {
+        matchType: "group",
+        winner: "Andrew Walden & Kristoffer ”K” Svensson",
+      },
+      {
+        matchType: "group",
+        winner: "Amy Deasismont & Uje Brandelius",
+      },
+      {
+        matchType: "group",
+        winner: "Amy Deasismont & Uje Brandelius",
+      },
+      {
+        matchType: "group",
+        winner: "Jakob Norrgård & Axel Åhman",
+      },
+      {
+        matchType: "semifinal",
+        semifinalFirst: "Lotta Lundgren & Erik Haag",
+        semifinalSecond: "Isobel Hadley-Kamptz & Kalle Lind",
+      },
+      {
+        matchType: "final",
+        winner: "Isobel Hadley-Kamptz & Kalle Lind",
+      },
+    ],
+  },
+  {
+    name: "Mackerian",
+    bets: [
+      {
+        matchType: "group",
+        winner: "Lotta Lundgren & Erik Haag",
+      },
+      {
+        matchType: "group",
+        winner: "Lotta Lundgren & Erik Haag",
+      },
+      {
+        matchType: "group",
+        winner: "Gry Forssell & Jonas Rhodiner",
+      },
+      {
+        matchType: "group",
+        winner: "Isobel Hadley-Kamptz & Kalle Lind",
+      },
+      {
+        matchType: "group",
+        winner: "Malin Persson Giolito & Ola Söderholm",
+      },
+      {
+        matchType: "group",
+        winner: "Isobel Hadley-Kamptz & Kalle Lind",
+      },
+      {
+        matchType: "group",
+        winner: "Kirsty Armstrong & Hanna Lublin Niklasson",
+      },
+      {
+        matchType: "group",
+        winner: "Kirsty Armstrong & Hanna Lublin Niklasson",
+      },
+      {
+        matchType: "group",
+        winner: "Andrew Walden & Kristoffer ”K” Svensson",
+      },
+      {
+        matchType: "group",
+        winner: "Amy Deasismont & Uje Brandelius",
+      },
+      {
+        matchType: "group",
+        winner: "Johanna Wagrell & Johan Hurtig",
+      },
+      {
+        matchType: "group",
+        winner: "Johanna Wagrell & Johan Hurtig",
+      },
+      {
+        matchType: "semifinal",
+        semifinalFirst: "Kirsty Armstrong & Hanna Lublin Niklasson",
+        semifinalSecond: "Lotta Lundgren & Erik Haag",
+      },
+      {
+        matchType: "final",
+        winner: "Kirsty Armstrong & Hanna Lublin Niklasson",
+      },
+    ],
+  },
+  {
+    name: "Slemmursconi belursconi",
+    bets: [
+      {
+        matchType: "group",
+        winner: "Lotta Lundgren & Erik Haag",
+      },
+      {
+        matchType: "group",
+        winner: "Bengt Skött & Ayan Jamal",
+      },
+      {
+        matchType: "group",
+        winner: "Bengt Skött & Ayan Jamal",
+      },
+      {
+        matchType: "group",
+        winner: "Isobel Hadley-Kamptz & Kalle Lind",
+      },
+      {
+        matchType: "group",
+        winner: "Malin Persson Giolito & Ola Söderholm",
+      },
+      {
+        matchType: "group",
+        winner: "Malin Persson Giolito & Ola Söderholm",
+      },
+      {
+        matchType: "group",
+        winner: "Kirsty Armstrong & Hanna Lublin Niklasson",
+      },
+      {
+        matchType: "group",
+        winner: "Assia Dahir & Kalle Berg",
+      },
+      {
+        matchType: "group",
+        winner: "Assia Dahir & Kalle Berg",
+      },
+      {
+        matchType: "group",
+        winner: "Amy Deasismont & Uje Brandelius",
+      },
+      {
+        matchType: "group",
+        winner: "Amy Deasismont & Uje Brandelius",
+      },
+      {
+        matchType: "group",
+        winner: "Jakob Norrgård & Axel Åhman",
+      },
+      {
+        matchType: "semifinal",
+        semifinalFirst: "Bengt Skött & Ayan Jamal",
+        semifinalSecond: "Assia Dahir & Kalle Berg",
+      },
+      {
+        matchType: "final",
+        winner: "Bengt Skött & Ayan Jamal",
+      },
+    ],
+  },
+  {
+    name: "Valle",
+    bets: [
+      {
+        matchType: "group",
+        winner: "Lotta Lundgren & Erik Haag",
+      },
+      {
+        matchType: "group",
+        winner: "Bengt Skött & Ayan Jamal",
+      },
+      {
+        matchType: "group",
+        winner: "Bengt Skött & Ayan Jamal",
+      },
+      {
+        matchType: "group",
+        winner: "Isobel Hadley-Kamptz & Kalle Lind",
+      },
+      {
+        matchType: "group",
+        winner: "Malin Persson Giolito & Ola Söderholm",
+      },
+      {
+        matchType: "group",
+        winner: "Malin Persson Giolito & Ola Söderholm",
+      },
+      {
+        matchType: "group",
+        winner: "Kirsty Armstrong & Hanna Lublin Niklasson",
+      },
+      {
+        matchType: "group",
+        winner: "Kirsty Armstrong & Hanna Lublin Niklasson",
+      },
+      {
+        matchType: "group",
+        winner: "Assia Dahir & Kalle Berg",
+      },
+      {
+        matchType: "group",
+        winner: "Amy Deasismont & Uje Brandelius",
+      },
+      {
+        matchType: "group",
+        winner: "Johanna Wagrell & Johan Hurtig",
+      },
+      {
+        matchType: "group",
+        winner: "Johanna Wagrell & Johan Hurtig",
+      },
+      {
+        matchType: "semifinal",
+        semifinalFirst: "Bengt Skött & Ayan Jamal",
+        semifinalSecond: "Malin Persson Giolito & Ola Söderholm",
+      },
+      {
+        matchType: "final",
+        winner: "Kirsty Armstrong & Hanna Lublin Niklasson",
+      },
+    ],
+  },
+  {
+    name: "Elin",
+    bets: [
+      {
+        matchType: "group",
+        winner: "Lotta Lundgren & Erik Haag",
+      },
+      {
+        matchType: "group",
+        winner: "Lotta Lundgren & Erik Haag",
+      },
+      {
+        matchType: "group",
+        winner: "Bengt Skött & Ayan Jamal",
+      },
+      {
+        matchType: "group",
+        winner: "Isobel Hadley-Kamptz & Kalle Lind",
+      },
+      {
+        matchType: "group",
+        winner: "Nils Holmqvist & Karin Magnusson",
+      },
+      {
+        matchType: "group",
+        winner: "Isobel Hadley-Kamptz & Kalle Lind",
+      },
+      {
+        matchType: "group",
+        winner: "Kirsty Armstrong & Hanna Lublin Niklasson",
+      },
+      {
+        matchType: "group",
+        winner: "Assia Dahir & Kalle Berg",
+      },
+      {
+        matchType: "group",
+        winner: "Assia Dahir & Kalle Berg",
+      },
+      {
+        matchType: "group",
+        winner: "Jakob Norrgård & Axel Åhman",
+      },
+      {
+        matchType: "group",
+        winner: "Johanna Wagrell & Johan Hurtig",
+      },
+      {
+        matchType: "group",
+        winner: "Jakob Norrgård & Axel Åhman",
+      },
+      {
+        matchType: "semifinal",
+        semifinalFirst: "Lotta Lundgren & Erik Haag",
+        semifinalSecond: "Jakob Norrgård & Axel Åhman",
+      },
+      {
+        matchType: "final",
+        winner: "Lotta Lundgren & Erik Haag",
+      },
+    ],
+  },
+  {
+    name: "Irma",
+    bets: [
+      {
+        matchType: "group",
+        winner: "Lotta Lundgren & Erik Haag",
+      },
+      {
+        matchType: "group",
+        winner: "Lotta Lundgren & Erik Haag",
+      },
+      {
+        matchType: "group",
+        winner: "Gry Forssell & Jonas Rhodiner",
+      },
+      {
+        matchType: "group",
+        winner: "Isobel Hadley-Kamptz & Kalle Lind",
+      },
+      {
+        matchType: "group",
+        winner: "Malin Persson Giolito & Ola Söderholm",
+      },
+      {
+        matchType: "group",
+        winner: "Malin Persson Giolito & Ola Söderholm",
+      },
+      {
+        matchType: "group",
+        winner: "Kirsty Armstrong & Hanna Lublin Niklasson",
+      },
+      {
+        matchType: "group",
+        winner: "Kirsty Armstrong & Hanna Lublin Niklasson",
+      },
+      {
+        matchType: "group",
+        winner: "Andrew Walden & Kristoffer ”K” Svensson",
+      },
+      {
+        matchType: "group",
+        winner: "Amy Deasismont & Uje Brandelius",
+      },
+      {
+        matchType: "group",
+        winner: "Amy Deasismont & Uje Brandelius",
+      },
+      {
+        matchType: "group",
+        winner: "Jakob Norrgård & Axel Åhman",
+      },
+      {
+        matchType: "semifinal",
+        semifinalFirst: "Lotta Lundgren & Erik Haag",
+        semifinalSecond: "Kirsty Armstrong & Hanna Lublin Niklasson",
+      },
+      {
+        matchType: "final",
+        winner: "Lotta Lundgren & Erik Haag",
+      },
+    ],
+  },
+  {
+    name: "Björne och Fanni",
+    bets: [
+      {
+        matchType: "group",
+        winner: "Gry Forssell & Jonas Rhodiner",
+      },
+      {
+        matchType: "group",
+        winner: "Lotta Lundgren & Erik Haag",
+      },
+      {
+        matchType: "group",
+        winner: "Gry Forssell & Jonas Rhodiner",
+      },
+      {
+        matchType: "group",
+        winner: "Isobel Hadley-Kamptz & Kalle Lind",
+      },
+      {
+        matchType: "group",
+        winner: "Malin Persson Giolito & Ola Söderholm",
+      },
+      {
+        matchType: "group",
+        winner: "Isobel Hadley-Kamptz & Kalle Lind",
+      },
+      {
+        matchType: "group",
+        winner: "Kirsty Armstrong & Hanna Lublin Niklasson",
+      },
+      {
+        matchType: "group",
+        winner: "Kirsty Armstrong & Hanna Lublin Niklasson",
+      },
+      {
+        matchType: "group",
+        winner: "Andrew Walden & Kristoffer ”K” Svensson",
+      },
+      {
+        matchType: "group",
+        winner: "Amy Deasismont & Uje Brandelius",
+      },
+      {
+        matchType: "group",
+        winner: "Amy Deasismont & Uje Brandelius",
+      },
+      {
+        matchType: "group",
+        winner: "Jakob Norrgård & Axel Åhman",
+      },
+      {
+        matchType: "semifinal",
+        semifinalFirst: "Isobel Hadley-Kamptz & Kalle Lind",
+        semifinalSecond: "Kirsty Armstrong & Hanna Lublin Niklasson",
+      },
+      {
+        matchType: "final",
+        winner: "Isobel Hadley-Kamptz & Kalle Lind",
+      },
+    ],
+  },
+];
